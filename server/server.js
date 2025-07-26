a59d2ba6bcbc404ddafc28eeb44650e25105b73d
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-07-26 13:14:30 +0100

    server configuration

--- server/server.js
+++ server/server.js
@@ -6,3 +6,11 @@ import 'dotenv/config';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('server is up and running'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
