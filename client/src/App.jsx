3bf08d84ca6fb62824ed953a4f83fe238ee077c1
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-07-30 16:54:10 +0100

    Token /app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,33 @@ import ReviewResume from './pages/ReviewResume';
 import RemoveObject from './pages/RemoveObject';
 import RemoveBackground from './pages/RemoveBackground';
 import GenerateImages from './pages/GenerateImages';
+import { useAuth } from '@clerk/clerk-react';
+import { useEffect } from 'react';
 
 const App = () => {
+
+  const { getToken } = useAuth();
+  useEffect(() => {
+    getToken().then((token) => {
+      console.log("Token retrieved successfully");
+    }).catch((error) => {
+      console.error("Error retrieving token:", error);
+    }
+    );
+  }, []);
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/ai' element={<Layout />}>
-          <Route index element= {<Dashboad/>}/>
-          <Route path='write-article' element={<WriteArticle />}/>
-          <Route path='blog-titles' element={<BlogTitles />}/>
-          <Route path='generate-images' element={<GenerateImages />}/>
-          <Route path='remove-object' element={<RemoveObject />}/>
-          <Route path='remove-background' element={<RemoveBackground />}/>
-          <Route path='review-resume' element={<ReviewResume />}/>
-          <Route path='community' element={<Community />}/>
+          <Route index element={<Dashboad />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path='community' element={<Community />} />
         </Route>
       </Routes>
     </div>
