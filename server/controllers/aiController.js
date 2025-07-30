5f51305b4702dd0ecdbd91eac1c88e7c0275d1ba
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-07-30 16:23:37 +0100

    openAI /Backend

---
+++ server/controllers/aiController.js
@@ -0,0 +1,35 @@
+import OpenAI from "openai";
+
+const AI = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
+
+export default generateArticle = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { prompt, length } = req.body;
+        const plan = req.plan;
+        const free_usage = req.free_usage;
+
+        if (plan !== 'premium' && free_usage >= 10) {
+            return res.json({
+                success: false,
+                message: "limit reached for free users, please upgrade to premium plan"
+            });
+        }
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: length,
+        });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message || 'Failed to generate article' });
+    }
+}
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,7 +4,7 @@ import { clerkClient } from "@clerk/express";
 
 export const auth = async (req, res, next) => {
     try {
-        const { userId, has } = await req.auth.auth();
+        const { userId, has } = await req.auth();
         const hasPremiumPlan = await has({ plan: 'premium' });
 
         const user = await clerkClient.users.getUser(userId);
