8df492f7190da0fd1ac8bfd617f54349d00b0ad7
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-08-03 17:48:48 +0100

    remove_background/object  /backend

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -145,4 +145,64 @@ export const generateImage = async (req, res) => {
     }
 }
 
+export const removeImageBackground = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { image } = req.files;
+        const plan = req.plan;
+
+
+        if (plan !== 'premium') {
+            return res.json({
+                success: false,
+                message: "this feature is only available for premium users"
+            });
+        }
+        const { secure_url } = await cloudinary.uploader.upload(image.path , {transformation: [{ effect: 'background_removal', background_removal: 'remove_the_background' }] });
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'remove background from the image',${secure_url},'image')`;
+
+
+        res.json({
+            success: true,
+            content: secure_url
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message || 'Failed to generate article' });
+    }
+}
+
+export const removeImageObject = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { object } = req.body;
+        const { image } = req.files;
+        const plan = req.plan;
+
+
+        if (plan !== 'premium') {
+            return res.json({
+                success: false,
+                message: "this feature is only available for premium users"
+            });
+        }
+        const { public_id } = await cloudinary.uploader.upload(image.path);
+
+        const imageUrl = cloudinary.url(public_id, { transformation: [{ effect: `gen_remove:${object}`, object_removal: object }] , resource_type: 'image' });
+
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},${`Removed ${object} from image`},${imageUrl},'image')`;
+
+
+        res.json({
+            success: true,
+            content: imageUrl
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message || 'Failed to generate article' });
+    }
+}
+
 
