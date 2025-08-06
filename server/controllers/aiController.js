ef61777d8374f61c46fc5bfd90d98f8d2fff7277
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-08-06 17:47:54 +0100

    review-resume  /route

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -3,6 +3,8 @@ import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+import pdf from 'pdf-parse/lib/pdf-parse.js'
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -225,13 +227,34 @@ export const reviewResume = async (req, res) => {
             })
         }
 
+        const dataBuffer = fs.readFileSync(resume.path)
+        const pdfData = await pdf(dataBuffer);
 
-        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},${`Removed ${object} from image`},${imageUrl},'image')`;
+        const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas of improvement. Resume
+        Content :\n\n${pdfData.text}`;
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-2.0-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_tokens: 1000,
+        });
+
+        const content = response.choices[0].message.content;
+
+
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'Review the uploaded resume',${content},'resume-review')`;
 
 
         res.json({
             success: true,
-            content: imageUrl
+            content
         });
     } catch (error) {
         console.log(error.message);
