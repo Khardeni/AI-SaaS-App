f2d0dad64d4178f5a809597855077292897e0495
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-08-06 18:01:36 +0100

    getUserCreations

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import { auth } from '../middlewares/auth.js';
-import { generateArticle, generateBlogTitle, generateImage } from '../controllers/aiController.js'
-
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, reviewResume } from '../controllers/aiController.js'
+import { upload } from '../configs/multer.js';
 
 const aiRouter = express.Router();
 
 aiRouter.post('/generate-article', auth, generateArticle);
 aiRouter.post('/generate-blog-title', auth, generateBlogTitle);
 aiRouter.post('/generate-article', auth, generateImage);
+aiRouter.post('/generate-image-background', auth,upload.single('image'), removeImageBackground);
+aiRouter.post('/generate-image-object', auth,upload.single('image'), removeImageObject);
+aiRouter.post('/generate-article', auth, reviewResume);
+
+
+
 
 export default aiRouter;
