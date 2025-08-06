f949686661789ea6d329f6cd66b7d34ec7cca37b
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-08-06 17:30:24 +0100

    review-resume  /backend

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -158,7 +158,7 @@ export const removeImageBackground = async (req, res) => {
                 message: "this feature is only available for premium users"
             });
         }
-        const { secure_url } = await cloudinary.uploader.upload(image.path , {transformation: [{ effect: 'background_removal', background_removal: 'remove_the_background' }] });
+        const { secure_url } = await cloudinary.uploader.upload(image.path, { transformation: [{ effect: 'background_removal', background_removal: 'remove_the_background' }] });
 
         await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'remove background from the image',${secure_url},'image')`;
 
@@ -177,7 +177,7 @@ export const removeImageObject = async (req, res) => {
     try {
         const { userId } = req.auth();
         const { object } = req.body;
-        const { image } = req.files;
+        const { image } = req.file;
         const plan = req.plan;
 
 
@@ -189,7 +189,41 @@ export const removeImageObject = async (req, res) => {
         }
         const { public_id } = await cloudinary.uploader.upload(image.path);
 
-        const imageUrl = cloudinary.url(public_id, { transformation: [{ effect: `gen_remove:${object}`, object_removal: object }] , resource_type: 'image' });
+        const imageUrl = cloudinary.url(public_id, { transformation: [{ effect: `gen_remove:${object}`, object_removal: object }], resource_type: 'image' });
+
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},${`Removed ${object} from image`},${imageUrl},'image')`;
+
+
+        res.json({
+            success: true,
+            content: imageUrl
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message || 'Failed to generate article' });
+    }
+}
+
+export const reviewResume = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const resume = req.file;
+        const plan = req.plan;
+
+
+        if (plan !== 'premium') {
+            return res.json({
+                success: false,
+                message: "this feature is only available for premium users"
+            });
+        }
+        if (resume > 5 * 1024 * 1024) {
+            return res.json({
+                success: false,
+                message: "resume file size exceeds allowed size (5MB)"
+            })
+        }
 
 
         await sql` INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},${`Removed ${object} from image`},${imageUrl},'image')`;
